37c93530582a39ed79d3ede8c654ef48ab10338a
OliverW10 <oliver.warrick2@gmail.com>
2024-09-05 15:51:01 +1000

    fix types

--- main.ts
+++ main.ts
@@ -3,78 +3,64 @@ import forceAtlas2 from "graphology-layout-forceatlas2";
 import circular from "graphology-layout/circular";
 import Sigma from "sigma"
 
-interface SubjectInfoDTO {
+interface SubjectInfo {
     subject_type: string;
     number: number;
     name: string;
     prereqs: number[];
     area: string;
-}
-
-interface SubjectInfoFilled extends SubjectInfoDTO {
     postreqs: number[];
     depth: number;
 }
 
-let subjectsCache: { [id: number] : SubjectInfoFilled } | undefined = undefined;
-async function getSubjects() : Promise<{ [id: number] : SubjectInfoFilled }> {
+let subjectsCache: Map<number, SubjectInfo> | undefined = undefined;
+async function getSubjects() : Promise<Map<number, SubjectInfo>> {
     if (subjectsCache) return subjectsCache;
 
     let response = await fetch("./subjects.json");
-    let subjectsList = await response.json() as SubjectInfoDTO[];
+    let subjectsList = await response.json() as SubjectInfo[];
 
-    // create "dict" to allow quick access to unfilled subject info's
-    let subjectsDtos = new Map<Number, SubjectInfoDTO>();
-    for (const dto of subjectsList) {
-        subjectsDtos[dto.number] = dto;
+    subjectsCache = new Map<number, SubjectInfo>();
+
+    for (const subj of subjectsList) {
+        subjectsCache.set(subj.number, subj);
+        subj.postreqs = [];
     }
 
     // maps a subject id to the subjects that have it as a prerequisite
-    let all_postreqs = new Map<Number, Number[]>();
-    for (const s of Object.values(subjectsDtos)) {
+    for (const s of subjectsCache.values()) {
         for (const currentPrereqId of s.prereqs) {
-            const other = all_postreqs[currentPrereqId];
-            if (!other) {
-                all_postreqs[currentPrereqId] = [];
+            const other = subjectsCache.get(currentPrereqId);
+            if (other == undefined) {
+                // console.warn(`Subject ${s.number} had a prereq on ${currentPrereqId} which couldnt be found`);
+                continue;
             }
-            all_postreqs[currentPrereqId].push(s.number);
+            other.postreqs.push(s.number);
         }
     }
     
     // Get prereq depth for each subject
-    let depths: { [ id: number]: number } = {};
-    for (const s of Object.values(subjectsDtos)) {
-        getDepth(s, subjectsDtos, depths);
-    }
-
-    subjectsCache = {};
-    for (const dto of Object.values(subjectsDtos)) {
-        const filled: SubjectInfoFilled = {
-            ...dto,
-            postreqs: all_postreqs[dto.number],
-            depth: depths[dto.number],
-        }
-        subjectsCache[dto.number] = filled;
+    for (const s of subjectsCache.values()) {
+        setDepth(s, subjectsCache);
     }
 
     return subjectsCache;
 }
 
-function getDepth(subject, all_subjects_dict, depths){
-    if (subject.prereqs.length == 0){
+function setDepth(subject: SubjectInfo | undefined, all_subjects_dict: Map<number, SubjectInfo>) : number {
+    if (subject == undefined || subject.prereqs.length == 0){
         return 0;
     }
 
-    if (depths[subject.number] != undefined) {
-        return depths[subject.number];
+    if (subject.depth != undefined) {
+        return subject.depth;
     }
-    // hack to prevent getting stuck on cycles
-    depths[subject.number] = 0;
 
-    const result = Math.max(...[subject.prereqs.map(id => {
-        return all_subjects_dict[id] ? getDepth(all_subjects_dict[id], all_subjects_dict, depths) : 0
-    })]) + 1;
-    depths[subject.number] = result;
+    // hack to prevent getting stuck on cycles, gets overwritten with actual value below
+    subject.depth = 0;
+
+    const result = Math.max(...subject.prereqs.map(id => setDepth(all_subjects_dict.get(id), all_subjects_dict))) + 1;
+    subject.depth = result;
     return result;
 }
 
@@ -82,20 +68,24 @@ function getDepth(subject, all_subjects_dict, depths){
 const renderGraph = async () => {
     var subjects = await getSubjects();
     const subjects_in_comp_sci = ['41173', '42913', '41004', '37233', '41182', '31005', '41052', '31748', '41080', '31256', '41043', '31269', '31261', '41175', '31097', '32011', '31338', '41905', '31777', '31266', '41900', '41076', '31277', '57304', '31282', '33130', '32009', '31257', '31280', '43024', '31260', '41184', '42050', '43030', '41180', '43023', '31016', '41181', '31255', '37262', '31080', '41019', '31263', '41113', '31927', '48433', '31276', '41903', '48436', '31250', '31245', '33116', '31272', '41891', '31243', '41889', '42028', '42037', '31265', '48033', '33230', '41025', '41171', '37161', '41077', '41183', '41001', '48024', '31268', '31247', '31258', '31262', '43025', '31275', '32146', '31251', '41174', '41185', '41890', '31253', '48450', '41021', '48730', '41040', '42036', '37181', '41020', '31271', '41092', '31242', '41039', '41172', '41026', '48270'];
-
-    var to_graph: SubjectInfoFilled[] = [];
-
+    
+    var to_graph: SubjectInfo[] = [];
+    
+    console.log(`total: ${subjects.size}, comp sci: ${subjects_in_comp_sci.length}`)
     for (var subjId of subjects_in_comp_sci) {
-        to_graph.push(subjects[parseInt(subjId)]);
+        var currentSubj = subjects.get(parseInt(subjId));
+        if (currentSubj == undefined) {
+            console.warn(`Subject ${subjId} from degree not found`);
+            continue;
+        }
+        to_graph.push(currentSubj);
     }
+    console.log(`valid: ${to_graph.length}`)
 
     const graph = new Graph();
 
     for (const subject of to_graph) {
-        try{
-            graph.addNode(subject.number, { label: subject.name, defaultLabelSize: 8, x: 0, y: subject.depth, size: 5 + subject.postreqs.length * 0.2, color: "blue" });
-        } catch {
-        }
+        graph.addNode(subject.number, { label: subject.name, defaultLabelSize: 8, x: 0, y: subject.depth, size: 5 + subject.postreqs.length * 0.2, color: "blue" });
     }
 
     for (const subject of to_graph) {
