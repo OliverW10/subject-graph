1d40f4de0f08e6fc6aa9a64c7128191c45644d15
OliverW10 <oliver.warrick2@gmail.com>
2024-09-15 22:13:30 +1000

    flesh out types

--- scraper/Program.fs
+++ scraper/Program.fs
@@ -1,16 +1,20 @@
 ﻿// For more information see https://aka.ms/fsharp-console-apps
 printfn "Hello from F#"
 
-let getPrereqExpressionForSubject subjectNumber = "1234 AND 9786"
+let getPrereqExpressionForSubject subjectNumber = Ok "(1234 AND 9786)"
+
+type NonEmptyList<'T> = 
+    | First of 'T
+    | Rest of list<'T>
 
 type ExpressionElement = 
     | SubjectId of int
-    | OpenScope
-    | CloseScope
-    | And
-    | Or
+    | And of NonEmptyList<ExpressionElement>
+    | Or of NonEmptyList<ExpressionElement>
 
-let parseExpression text = [ OpenScope; SubjectId(1234) ]
+let allowedChars = "()1234567890"
+
+let parseExpression (text: string) = Ok (String.collect(fun c -> if allowedChars.Contains(c) then c else '' ))
 
 type TruthRow = {
     inputs: bool[]
@@ -21,17 +25,21 @@ type TruthTable = {
     rows: seq<TruthRow>
 }
 
-let calculateTruthTable expression = { TruthTable.rows = Seq.empty }
+let calculateTruthTable (expression: ExpressionElement) = Ok { TruthTable.rows = Seq.empty }
 
 type Prerequisite = 
     | Required of subjectNumber: int
     | Optioanl of subjectNumber: int * alsoRequired: Option<Prerequisite>
 
-let getPrereqs truthTable = seq { Prerequisite.Required }
+let getPrereqs (truthTable: TruthTable) = Ok [ Prerequisite.Required ]
 
 let getPrereqsForSubject subjectNumber =
     subjectNumber
     |> getPrereqExpressionForSubject
-    |> parseExpression
-    |> calculateTruthTable
-    |> getPrereqs
+    |> Result.bind parseExpression
+    |> Result.bind calculateTruthTable
+    |> Result.bind getPrereqs
+    |> fun result ->
+        match result with 
+        | Ok prereqs -> printfn String.concat(", ", prereqs.map(fun p -> p.ToString()))
+        | Error errMsg -> printfn errMsg
